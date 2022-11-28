20fb63bba73c03a078ddb1dbfeb0375e348b86aa
RafalKieszek <119293109+RafalKieszek@users.noreply.github.com>
2022-11-28 10:27:17 +0100

    Update rastringis.m

--- rastringis.m
+++ rastringis.m
@@ -1,5 +1,5 @@
 function [y]=rastringis(x)
-%% min=0 dla x=[delta,delta]([10,10])
+%% min=0 for x=[delta,delta]([10,10])
 ddx=10;
 x1=x(1);
 x2=x(2);
@@ -9,8 +9,6 @@ y=zeros(length(x1),length(x2));
 for i=1:dx1
     for j=1:dx2
         y(i,j)=(20+(x1(i)-ddx).^2+(x2(j)-ddx).^2)-10*(cos(2*pi*(x1(i)-ddx))+cos(2*pi*(x2(j)-ddx)));
-       %       (20+(x1(i)-2).^2+(x2(j)-2).^2)-5*(cos(2*pi*(x1(i)))+cos(2*pi*(x2(j))))
-       %y(i,j)=10+x1(i)^2-10*cos(2*pi*x1(i))+x2(i)^2-10*cos(2*pi*x2(i));
     end
 end
 end
